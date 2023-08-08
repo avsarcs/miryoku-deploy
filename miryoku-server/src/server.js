bc9f981ae263eecfffef4f65087c0fa0d6135681
Mehmet Emin Avsar <avsarmehmetemin@gmail.com>
2023-08-08 21:32:39 +0300

    Made the relative path from server access the front end

--- miryoku-server/src/server.js
+++ miryoku-server/src/server.js
@@ -32,7 +32,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', '..index.html'));
+    res.sendFile(path.join(__dirname, 'build', '../../miryoku-front-end/public/index.html'));
 });
 
 // routes
